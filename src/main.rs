79c03d54295761059129e72266468d89b8f91ef3
Kristiyan Yochev <kris.yochev@gmail.com>
2023-08-29 03:00:27 +0300

    Added a function for displaying a game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 mod core;
 
+use core::Game;
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new(10, 10, 10).unwrap();
+    show_game(&game);
+}
+
+fn show_game(game: &Game) {
+    use core::CellState;
+
+    let print_separator_line = || {
+        for _ in 0..game.field_width() {
+            print!("+-");
+        }
+        println!("+");
+    };
+
+    print_separator_line();
+
+    for y in 0..game.field_height() {
+        for x in 0..game.field_width() {
+            print!("|");
+            match game.cell_at((x, y)).unwrap() {
+                CellState::Hidden => print!("#"),
+                CellState::Flagged => print!("!"),
+                CellState::Revealed(0) => print!(" "),
+                CellState::Revealed(n) => print!("{}", n),
+            }
+        }
+        println!("|");
+        print_separator_line();
+    }
 }
