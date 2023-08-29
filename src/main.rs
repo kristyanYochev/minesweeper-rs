895ee049135bf633f1c8cd541f28a87524064308
Kristiyan Yochev <kris.yochev@gmail.com>
2023-08-29 03:45:33 +0300

    Added gameplay loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,108 @@
 mod core;
 
-use core::Game;
+use core::{Game, RevealResult};
 use std::io::{self, Write};
 
 fn main() {
-    let mut game = init_game();
-    clear_screen();
-    show_game(&game);
+    let game = init_game();
+    game_loop(game);
+}
+
+fn game_loop(mut game: Game) {
+    loop {
+        clear_screen();
+        show_game(&game);
+
+        match read_command() {
+            Command::Reveal(x, y) => match game.reveal((x, y)) {
+                Ok(RevealResult::Continue) => {}
+                Ok(RevealResult::GameOver) => {
+                    println!("You lost :(");
+                    return;
+                }
+                Ok(RevealResult::Win) => {
+                    println!("Congratulations! You won!");
+                    return;
+                }
+                Err(e) => {
+                    println!("{e}");
+                }
+            },
+            Command::ToggleFlag(x, y) => match game.toggle_flag((x, y)) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("{e}");
+                }
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Command {
+    Reveal(usize, usize),
+    ToggleFlag(usize, usize),
+}
+
+fn read_command() -> Command {
+    loop {
+        show_prompt();
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Cannot read from stdin!");
+
+        let parts: Vec<_> = buffer.split(' ').collect();
+
+        match parts[0] {
+            "r" | "reveal" => {
+                let x = match parts[1].trim().parse::<usize>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("`{}` is not a number", parts[1]);
+                        continue;
+                    }
+                };
+
+                let y = match parts[2].trim().parse::<usize>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("`{}` is not a number", parts[2]);
+                        continue;
+                    }
+                };
+
+                return Command::Reveal(x, y);
+            }
+            "f" | "flag" => {
+                let x = match parts[1].trim().parse::<usize>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("`{}` is not a number", parts[1]);
+                        continue;
+                    }
+                };
+
+                let y = match parts[2].trim().parse::<usize>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("`{}` is not a number", parts[2]);
+                        continue;
+                    }
+                };
+
+                return Command::ToggleFlag(x, y);
+            }
+            _ => {
+                println!("{} is not a command", parts[0]);
+            }
+        }
+    }
+}
+
+fn show_prompt() {
+    print!("\n\n> ");
+    io::stdout().flush().expect("Cannot flush stdout!");
 }
 
 fn clear_screen() {
